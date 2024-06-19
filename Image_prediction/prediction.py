5338407b749c4ffcf22380ab4bdeb0f8df79ceb9
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2024-06-19 17:23:02 +0100

    Add files via upload

---
+++ Image_prediction/prediction.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ["Plane", "Car", "Bird", " Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+
+training_images = training_images[:20000]
+training_labels =  training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+model = models.load_model('image_classifier.keras')
+
+img = cv.imread('car.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+
+prediction = model.predict(np.array([img]) / 255)
+index = (np.argmax(prediction))
+print(f"Prediction is {class_names[index]}")
+
+plt.show()
